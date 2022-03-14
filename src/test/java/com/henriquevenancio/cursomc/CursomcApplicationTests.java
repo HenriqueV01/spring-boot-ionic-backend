30d10f9c6125857af8360745c6f2a75326191e0e
Henrique <henrique.unibratec01@gmail.com>
2022-03-14 08:41:31 -0300

    Projeto criado

---
+++ src/test/java/com/henriquevenancio/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriquevenancio.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
