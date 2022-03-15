01bf1409dcbd833dfb07147e8b60a4a857eecf55
Henrique <henrique.unibratec01@gmail.com>
2022-03-15 11:38:11 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/henriquevenancio/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.henriquevenancio.cursomc.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.henriquevenancio.cursomc.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler{	
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+	}
+
+}
